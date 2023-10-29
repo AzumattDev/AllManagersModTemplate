9f8a464b9c7dc653af53b232fa3edfaa35f858c0
AzumattDev <azumattgaming@gmail.com>
2023-10-29 02:38:43 -0400

    Update for Lastest BepInEx not having unstripped_corlib folder anymore, ItemManager, Add Keyboard Extensions

--- Plugin.cs
+++ Plugin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using BepInEx;
 using BepInEx.Configuration;
@@ -429,5 +430,18 @@ namespace AllManagersModTemplate
         }
 
         #endregion
+    }
+
+    public static class KeyboardExtensions
+    {
+        public static bool IsKeyDown(this KeyboardShortcut shortcut)
+        {
+            return shortcut.MainKey != KeyCode.None && Input.GetKeyDown(shortcut.MainKey) && shortcut.Modifiers.All(Input.GetKey);
+        }
+
+        public static bool IsKeyHeld(this KeyboardShortcut shortcut)
+        {
+            return shortcut.MainKey != KeyCode.None && Input.GetKey(shortcut.MainKey) && shortcut.Modifiers.All(Input.GetKey);
+        }
     }
 }
